1ff41f4c4a0b322f2097fd8e7eb2e91a38e0324c
Ballo Developer <ballo@example.com>
2026-09-24 21:40:08 +0300

    Change default local PORT to 3005 to prevent port 3000 conflicts

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
   cors: { origin: '*' }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 process.on('uncaughtException', (err) => {
   console.error('[Uncaught Exception]:', err);
